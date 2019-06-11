6b200dc9b4e6aca5d0cb9ba8f1d4c3c97f876b33
Benji Cohen <Benji.cohen@iprospect.com>
2019-06-11 10:44:56 +0100

    1. Hello World exercise and example built

---
+++ Code/1. Hello World/example.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Explanation
+# We use the `print()` function to log a message to screen the message can be a
+# string, or any other object, the object will be converted into a string before
+# being written to the screen
+# ------------------------------------------------------------------------------
+print("Print me fam")
+# ------------------------------------------------------------------------------
+# The JS equivalent would be `console.log("Print me fam")`
+# ------------------------------------------------------------------------------
