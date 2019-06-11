49b14932c1e7660de4657150ebcaae2add158a54
Benji Cohen <Benji.cohen@iprospect.com>
2019-06-11 15:58:08 +0100

    3. Lists - Exercise created

---
+++ Code/3. Lists/exercise.py
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------
+# Exercise
+# 1. Add the grapes and apples variables to the list
+# 2. Remove the value "75" from the list
+# 3. Join myList with bobsList together, removing the duplicate values
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 1. Add the grapes and apples variables to the list
+myList = []
+grapes = "Grapes"
+apples = "Apples"
+print(myList)
+
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 2. Remove the value "75" from the list and replace it with the string "Kiwi"
+myList = ['Apples', 'Grapes', 75, "Oranges"]
+print(myList)
+
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 3. Join myList with bobsList together, removing the duplicate values
+myList = [1, 2, 3, 4]
+bobsList = [4, 5, 6, 7]
+print(myList)
