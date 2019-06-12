e87aa6e053961e5bd843477d16c3a165e9da199b
Benji Cohen <Benji.cohen@iprospect.com>
2019-06-12 09:13:56 +0100

    4. Basic Operators - Exercise built

---
+++ Code/4. Basic Operators/exercise.py
@@ -0,0 +1,58 @@
+# ------------------------------------------------------------------------------
+# Exercise
+# 1. Amend the value of c below so each print statement prints true
+# 2. Using string operators, amend the code to print:
+	# Give it away give it away give it away now
+	# Give it away give it away give it away now
+	# Give it away give it away give it away now
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 1. Amend the value of c below so each print statement prints true
+
+a, b = 1, 3
+c = a - b
+print(c == 4)
+
+a, b = 5, 10
+c = a + b
+print(c == 50)
+
+a, b = 11, 2 
+c = a / b
+print(c == 1)
+
+a, b == 17, 3
+c = 17 / 3
+print(c == 4913)
+
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 2. Using string operators, amend the code to print:
+	# Give it away give it away give it away now
+	# Give it away give it away give it away now
+	# Give it away give it away give it away now
+
+giveItAway = "Give it away"
+now = "now"
+chorus = giveItAway + now
+print(chorus)
+
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 3. Using list operators, amend the row variables so it prints:
+	# ['O', 'X', 'O']
+	# ['X', 'O', 'X']
+	# ['O', 'X', 'X']
+	
+X = ['X']
+O = ['O']
+row1 = ['O']
+row2 = ['X', 'O']
+row3 = ['X', 'X']
+
+print(row1)
+print(row2)
+print(row3)
