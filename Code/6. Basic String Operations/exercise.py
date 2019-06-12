f9232522752c7f53612bdcbfcf7fd04bc1f5ea69
Benji Cohen <Benji.cohen@iprospect.com>
2019-06-12 11:27:03 +0100

    6. Basic String Operations - Exercise built

---
+++ Code/6. Basic String Operations/exercise.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# Exercise
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 1. Write out some additional code to capitalise the first character of each
+# word in the string, the print statement should print "Hello, World!"
+string = "hello, world!"
+titleCaseString = string
+print(titleCaseString)
