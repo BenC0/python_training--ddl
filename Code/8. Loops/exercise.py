a0a0f910cab59564fa093b3c187b39e1db3ada36
Benji Cohen <Benji.cohen@iprospect.com>
2019-06-13 16:20:42 +0100

    8. Loops - Exercise built

---
+++ Code/8. Loops/exercise.py
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------------------------
+# Exercise
+# 1. Fizzbuzz!
+# ------------------------------------------------------------------------------
+
+# Exercise
+# 1. Fizzbuzz!
+# Amend the below for loop so that it:
+# - Prints "Fizz" when x is a multiple of 3
+# - Prints "Buzz" when x is a multiple of 5
+# - Prints "FizzBuzz" when x is a multple of 3 and 5
+# - Prints the number if the number is not a multiple of 3 or 5
+# Note: You'll want to use the Modulo operator (%) from 4. Basic Operators
+
+for x in range(1, 21):
+	print(x)
